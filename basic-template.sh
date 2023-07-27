dc287a9e4a2a2bf5700397ec66139ad2fe6e3c97
Krishna Kaushal <xalgord@gmail.com>
2023-07-27 12:52:19 +0530

    Update basic-template.sh

--- basic-template.sh
+++ basic-template.sh
@@ -2,13 +2,10 @@
 
 # This script was written to scan CVE-2023-24488 vulnerability
 targets="$1"
-
+mkdir ~/targets/result
 while IFS= read -r url; do
-	echo "creating directory for $url"
-	if [ -d "~/targets/$url" ]; then
-		echo "Folder $url already exists."
-	else
-		mkdir ~/targets/$url
-	fi
-	echo "reached"
+	echo "Performing subdomain enumeration on $url"
+	subfinder -d $url -all -o ~/targets/result/$url.txt
+	echo "Running Nuclei on $url"
+	cat ~/targets/result/$url.txt | nuclei -t ~/nuclei-templates/CVE-2023-24488.yaml | notify
 done < "$targets"
