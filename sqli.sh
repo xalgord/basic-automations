56e97fae53b828e514de7ddc9c16d7ad6b7aae09
Krishna Kaushal <xalgord@gmail.com>
2023-07-29 14:31:14 +0530

    Update sqli.sh

--- sqli.sh
+++ sqli.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 targets="$1"
-output_folder=~/targets/scan_result
+folder_name="$2"
+output_folder=~/targets/$folder_name
 mkdir "$output_folder"
 
 while IFS= read -r url; do
@@ -12,19 +13,19 @@ while IFS= read -r url; do
 	echo -e "\n\033[1;36mRunning Paramspider on $url\033[0m"
 	paramspider.py -d "$url" --level high -o "$output_folder/paramspider.txt"
 	echo -e "\033[1;33mRunning sqlidetector on paramspider.txt\033[0m"
-	python3 ~/tools/SQLiDetector/sqlidetector.py -f $output_folder/paramspider.txt -w 10 -t 10 | notify
+	python3 ~/tools/SQLiDetector/sqlidetector.py -f $output_folder/paramspider.txt -w 50 -t 10 | notify
 	
 	# Run Gau on the URL
 	echo -e "\n\033[1;36mRunning Gau on $url\033[0m"
 	gau "$url" --o "$output_folder/gau.txt"
 	echo -e "\033[1;33mRunning sqlidetector on gau.txt\033[0m"
-	python3 ~/tools/SQLiDetector/sqlidetector.py -f $output_folder/paramspider.txt -w 10 -t 10 | notify
+	python3 ~/tools/SQLiDetector/sqlidetector.py -f $output_folder/gau.txt -w 50 -t 10 | notify
 	
 	# Run waybackurls on the URL
 	echo -e "\n\033[1;36mRunning waybackurls on $url\033[0m"
 	waybackurls "$url" > "$output_folder/waybackurls.txt"
 	echo -e "\033[1;33mRunning sqlidetector on waybackurls.txt\033[0m"
-	python3 ~/tools/SQLiDetector/sqlidetector.py -f $output_folder/paramspider.txt -w 10 -t 10 | notify
+	python3 ~/tools/SQLiDetector/sqlidetector.py -f $output_folder/waybackurls.txt -w 50 -t 10 | notify
 	
 	echo -e "\n\033[1;32mTask Finished!!!\033[0m"
 	
