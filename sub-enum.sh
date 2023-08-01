d1a0e425e5d1aa065059d0a7fdb13fa20cc377f8
Krishna Kaushal <xalgord@gmail.com>
2023-08-01 14:39:21 +0530

    Update sub-enum.sh

--- sub-enum.sh
+++ sub-enum.sh
@@ -1,35 +1,81 @@
 #!/bin/bash
 
-target="$1"
-output_folder="$target"
+output_folder=""
+
+while getopts ":d:f:" opt; do
+    case $opt in
+        d)
+            target="$OPTARG"
+            output_folder="$target"
+            ;;
+        f)
+            input_file="$OPTARG"
+            if [ ! -f "$input_file" ]; then
+                echo "Error: Input file not found."
+                exit 1
+            fi
+            output_folder="${input_file%.*}"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$target" ] && [ -z "$input_file" ]; then
+    echo "Usage: $0 [-d domain] [-f file.txt]"
+    exit 1
+fi
+
 if [ ! -d "$output_folder" ]; then
     mkdir "$output_folder"
     echo "Folder '$output_folder' created successfully."
-grep_domain=$(echo "$target" | grep -oE '[^.]+[.][^.]+$' | awk -F '.' '{print $NF}')
 fi
-# Color variables
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
 
-echo -e "${GREEN}Running Subfinder on $target${NC}"
-# subfinder -d "$target" -all -o "$output_folder/subfinder.txt"
+if [ -n "$target" ]; then
+    # If target is provided directly, process it as a single domain
+    grep_domain=$(echo "$target" | grep -oE '[^.]+[.][^.]+$' | awk -F '.' '{print $NF}')
+    domains=("$target")
+else
+    # If input file is provided, read domains from the file
+    grep_domain=""
+    mapfile -t domains < "$input_file"
+fi
 
-echo -e "${GREEN}Running assetfinder on $target${NC}"
-# assetfinder "$target" | grep "$target" > "$output_folder/assetfinder.txt"
+# Process each domain one by one
+for target in "${domains[@]}"; do
+    echo -e "\nProcessing domain: $target"
 
-echo -e "${GREEN}Running Amass on $target${NC}"
-# amass enum -passive -d "$target" -o "$output_folder/amass.txt"
 
-echo -e "${GREEN}Running Knockpy on $target${NC}"
-knockpy "$target" | grep -oE '[[:alnum:].-]+\.$grep_domain' > "$output_folder/knockpy.txt"
+    echo -e "${GREEN}Running Subfinder on $target${NC}"
+    subfinder -d "$target" -all -o "$output_folder/$target-subfinder.txt"
 
-echo -e "${GREEN}Removing Duplicates and Combining all files to all.txt${NC}"
-cat "$output_folder"/* | sort -u > "$output_folder/all.txt"
+    echo -e "${GREEN}Running assetfinder on $target${NC}"
+    assetfinder "$target" | grep "$target" > "$output_folder/$target-assetfinder.txt"
 
-echo -e "${GREEN}Checking for live domains${NC}"
-cat "$output_folder/all.txt" | httpx > "$output_folder/httpx.txt"
-echo -e "${GREEN}Stored all live URLs to $output_folder/httpx.txt${NC}"
+    echo -e "${GREEN}Running Amass on $target${NC}"
+    amass enum -passive -d "$target" -o "$output_folder/$target-amass.txt"
 
-echo -e "${YELLOW}Subdomain Enumeration Finished${NC}"
+    echo -e "${GREEN}Running Knockpy on $target${NC}"
+    knockpy "$target" | grep -oE '[[:alnum:].-]+\.$grep_domain' > "$output_folder/$target-knockpy.txt"
+
+done
+    echo -e "${GREEN}Removing Duplicates and Combining all files to all.txt${NC}"
+    cat "$output_folder/"* | sort -u > "$output_folder/all.txt"
+
+    echo -e "${GREEN}Checking for live domains${NC}"
+    cat "$output_folder/all.txt" | httpx > "$output_folder/httpx.txt"
+    echo -e "${GREEN}Stored all live URLs to $output_folder/httpx.txt${NC}"
+
+
+echo "Removing Unwanted Files..."
+for target in "${domains[@]}"; do
+	rm -rf "$output_folder/$target"*
+done
+
+echo -e "\n${YELLOW}Subdomain Enumeration Finished${NC}"
