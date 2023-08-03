8bba3efba9bfbc22697acc07e942b6825d34daa3
Krishna Kaushal <xalgord@gmail.com>
2023-08-03 13:49:41 +0530

    Update xss.sh

--- xss.sh
+++ xss.sh
@@ -1,53 +1,41 @@
 #!/bin/bash
 
-if [[ "$1" == "--help" ]]; then
-    echo "Usage: $0 <targets_file> <folder_name>"
-    echo "       <targets_file>: File containing a list of target URLs."
-    echo "       <folder_name>: Name of the output folder where results will be stored."
-    exit 0
-fi
-
-if [[ $# -ne 2 ]]; then
-    echo "Error: Incorrect number of arguments. Use --help for usage information."
-    exit 1
-fi
-
 targets="$1"
 folder_name="$2"
-output_folder=~/targets/"$folder_name"
+output_folder=$folder_name
 mkdir "$output_folder"
 
 while IFS= read -r url; do
 
-    echo -e "\n\033[1;35mScanning $url\033[0m"
-
-    # Run Paramspider on the URL
-    echo -e "\n\033[1;36mRunning Paramspider on $url\033[0m"
-    paramspider.py -d "$url" --level high -o "$output_folder/paramspider.txt"
-    echo -e "\033[1;33mRunning kxss on paramspider.txt\033[0m"
-    cat "$output_folder/paramspider.txt" | kxss | grep -E "['\"<>].*['\"<>].*['\"<>].*['\"<>]" | notify
-
-    # # Run Gau on the URL
-    # echo -e "\n\033[1;36mRunning Gau on $url\033[0m"
-    # gau "$url" --o "$output_folder/gau.txt"
-    # echo -e "\033[1;33mRunning kxss on gau.txt\033[0m"
-    # cat "$output_folder/gau.txt" | kxss | grep -E "['\"<>].*['\"<>].*['\"<>].*['\"<>]" | notify
-
-    # # Run waybackurls on the URL
-    # echo -e "\n\033[1;36mRunning waybackurls on $url\033[0m"
-    # waybackurls "$url" > "$output_folder/waybackurls.txt"
-    # echo -e "\033[1;33mRunning kxss on waybackurls.txt\033[0m"
-    # cat "$output_folder/waybackurls.txt" | kxss | grep -E "['\"<>].*['\"<>].*['\"<>].*['\"<>]" | notify
-
-    # Run Katana on the URL
-    # echo -e "\n\033[1;36mRunning Katana on $url\033[0m"
-    # katana -u "$url" -d 5 -jc -headless -no-sandbox -kf robotstxt,sitemapxml -output "$output_folder/katana.txt"
-    # echo -e "\033[1;33mRunning kxss on katana.txt\033[0m"
-    # cat "$output_folder/katana.txt" | kxss | grep -E "['\"<>].*['\"<>].*['\"<>].*['\"<>]" | notify
-
-    echo -e "\n\033[1;32mTask Finished!!!\033[0m"
-
-    echo -e "\033[1;31mRemoving Files\033[0m"
-    rm -rf "$output_folder"/*
-
+	echo -e "\n\033[1;35mScanning $url\033[0m"
+	
+	# Run Paramspider on the URL
+	echo -e "\n\033[1;36mRunning Paramspider on $url\033[0m"
+	paramspider.py -d "$url" --level high -o "$output_folder/paramspider.txt"
+	echo -e "\033[1;33mRunning kxss on paramspider.txt\033[0m"
+	cat "$output_folder/paramspider.txt" | kxss | grep -E "['\"<>].*['\"<>].*['\"<>].*['\"<>]" | notify
+	
+	# # Run Gau on the URL
+	# echo -e "\n\033[1;36mRunning Gau on $url\033[0m"
+	# gau "$url" --o "$output_folder/gau.txt"
+	# echo -e "\033[1;33mRunning kxss on gau.txt\033[0m"
+	# cat "$output_folder/gau.txt" | kxss | grep -E "['\"<>].*['\"<>].*['\"<>].*['\"<>]" | notify
+	
+	# # Run waybackurls on the URL
+	# echo -e "\n\033[1;36mRunning waybackurls on $url\033[0m"
+	# waybackurls "$url" > "$output_folder/waybackurls.txt"
+	# echo -e "\033[1;33mRunning kxss on waybackurls.txt\033[0m"
+	# cat "$output_folder/waybackurls.txt" | kxss | grep -E "['\"<>].*['\"<>].*['\"<>].*['\"<>]" | notify
+	
+	# Run Katana on the URL
+	# echo -e "\n\033[1;36mRunning Katana on $url\033[0m"
+	# katana -u "$url" -d 5 -jc -headless -no-sandbox -kf robotstxt,sitemapxml -output "$output_folder/katana.txt"
+	# echo -e "\033[1;33mRunning kxss on katana.txt\033[0m"
+	# cat "$output_folder/katana.txt" | kxss | grep -E "['\"<>].*['\"<>].*['\"<>].*['\"<>]" | notify
+	
+	echo -e "\n\033[1;32mTask Finished!!!\033[0m"
+	
+	echo -e "\033[1;31mRemoving Files\033[0m"
+	rm -rf "$output_folder"/*
+	
 done < "$targets"
